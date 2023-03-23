ca72f80ea88672a4eba7e0e1d3e6c968dcedde55
Rahul Kumawat <116284606+kumawat30rahul@users.noreply.github.com>
2023-03-23 10:45:20 +0530

    sdf

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React,{useEffect} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data,setData] = useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => setData(data))
+  },[])
   return (
     <div>
-        {/* Do not remove the main div */}
+        {data && data.map((item,index) => (
+          <div>
+          <h1>{index+1}.{item.title}</h1>
+          <p>{item.body}</p>
+          </div>
+        ))}
     </div>
   )
 }
